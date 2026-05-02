cfa41df3a09a0edf3e022db0df92e6f2fd550c6a
Gaurav Joshi <gauravj.omak@gmail.com>
2026-05-02 16:54:06 +0530

    feat: add AdminSeeder and TagSeeder to DatabaseSeeder execution flow

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        //run admin seeder and tag seeder here
+        $this->call(AdminSeeder::class);
+        $this->call(TagSeeder::class);
     }
 }
